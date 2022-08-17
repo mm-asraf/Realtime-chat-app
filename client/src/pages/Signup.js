ebacd3869aa69a92ce1df304349c95f3dee5084f
Mahboob Asraf <mahboobasraf@gmail.com>
2022-08-17 14:39:27 +0530

    signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import "./Signup.css";
+import { Link } from "react-router-dom";
+import botImage from "../assets/bot.jpeg";
+
+const validateImage = {};
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col
+          md={7}
+          className="d-flex align-items-center justify-content-center flex-direction-column"
+        >
+          <Form style={{ widht: "80%", maxWidth: 500 }}>
+            <h1 className="text-center">Create Account</h1>
+            <div className="signup-profile-pic__container">
+              <img src={botImage} className="signup-profile-pic" alt="" />
+              <label htmlFor="imgae-upload" className="image-upload-label">
+                <i className="fas fa-plus-circle add-picture-icon"></i>
+              </label>
+              <input
+                type="file"
+                id="image-upload"
+                hidden
+                accept="image/jpeg, image/png"
+                onChange={validateImage}
+              />
+            </div>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Your Name"
+                onChange={handleName}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Your Email"
+                onChange={handleEmail}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter Your password"
+                onChange={handlePassword}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+            <div className="py-4">
+              <p className="text-center">
+                Already have an account ? <Link to="/login">Login</Link>
+              </p>
+            </div>
+          </Form>
+        </Col>
+        <Col md={5} className="signup__bg"></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Signup;
