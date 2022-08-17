bd4c6f8c83b59dbf6d0f0d08257024647dcf6b6e
Mahboob Asraf <mahboobasraf@gmail.com>
2022-08-17 13:59:48 +0530

    home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Row>
+      <Col
+        md={6}
+        className="d-flex flex-direction-column align-items-center justify-content-center"
+      >
+        <div>
+          <h1>Share the world with your friends</h1>
+          <p>Chat App lets you connect with the world</p>
+          <LinkContainer to="/chat">
+            <Button varient="success">
+              Get Started{" "}
+              <i className="home-message-icon fas fa-comments home-message-icons"></i>
+            </Button>
+          </LinkContainer>
+        </div>
+      </Col>
+      <Col md={6} className="home__bg"></Col>
+    </Row>
+  );
 };
 
 export default Home;
